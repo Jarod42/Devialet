2d2f7ecfe8f0a3cbf5014816bb182108e3952c81
Jarod42 <Jarod42@users.noreply.github.com>
2023-03-01 19:57:52 +0100

    Add initial (empty) files.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+![badge](https://github.com/Jarod42/Diavalet/workflows/ubuntu/badge.svg)
+
+Small project for Diavalet interview.
---
+++ premake/premake5.lua
@@ -0,0 +1,52 @@
+local Root = path.getabsolute("..")
+
+if (_ACTION == nil) then
+	return
+end
+
+local LocationDir = path.join(Root, "solution", _ACTION)
+
+workspace "Project"
+	location(LocationDir)
+	configurations {"Debug", "Release"}
+
+	objdir(path.join(LocationDir, "obj")) -- premake adds $(configName)/$(AppName)
+	targetdir(path.join(LocationDir, "bin", "%{cfg.buildcfg}"))
+	startproject "app"
+
+group "3rd"
+	project "doctest"
+		kind "None"
+		files { path.join(Root, "3rd/doctest/**.*") }
+group ""
+
+project "app"
+	kind "ConsoleApp"
+	cppdialect "C++20"
+
+	targetname("iplayer")
+	files { path.join(Root, "src/app/**.*") }
+	includedirs { path.join(Root, "src/lib") }
+	links { "iplayerlib" }
+
+project "lib"
+	kind "StaticLib"
+	cppdialect "C++20"
+
+	targetname("iplayerlib")
+	files {path.join(Root, "src/lib/**.*")}
+
+project "test"
+	kind "ConsoleApp"
+	cppdialect "C++20"
+
+	targetname("test")
+	files { path.join(Root, "test/**.*") }
+	includedirs { path.join(Root, "src/lib") }
+	includedirs { path.join(Root, "3rd/doctest") }
+	links { "iplayerlib" }
+
+project "data"
+	kind "None"
+
+	files { path.join(Root, "data/**.*") }
