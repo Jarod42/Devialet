d4dcc978f4c0e6aaa251c64cc5f181ef611dafb4
Jarod42 <Jarod42@users.noreply.github.com>
2023-03-03 11:42:58 +0100

    Improve `shell`: - more verbose - select more friendly path (add command cd)

--- src/lib/shell.cpp
+++ src/lib/shell.cpp
@@ -8,7 +8,21 @@
 namespace
 {
 //------------------------------------------------------------------------------
-void showHelp(iplayer::Player&, std::ostream& os, std::istream&);
+void showHelp(iplayer::Player&, std::ostream&, std::istream&);
+
+//------------------------------------------------------------------------------
+void cd(iplayer::Player&, std::ostream& os, std::istream& is)
+{
+	os << "Current path was " << std::filesystem::current_path() << "\n";
+	std::string directory;
+	if (std::getline(is, directory)) {
+		std::filesystem::current_path(directory);
+	} else {
+		os << "Invalid argument\n";
+	}
+	os << "Current path is " << std::filesystem::current_path() << "\n";
+}
+
 
 //------------------------------------------------------------------------------
 void add_track(iplayer::Player& player, std::ostream& os, std::istream& is)
@@ -16,10 +30,13 @@ void add_track(iplayer::Player& player, std::ostream& os, std::istream& is)
 	std::string filename;
 	is >> filename;
 	try {
+		os << "Adding track " << filename << "\n";
 		player.push_back(iplayer::openTrack(filename));
+		os << "Added\n";
 	}
 	catch (const std::exception& ex) {
-		os << ex.what();
+		os << ex.what() << "\n";
+		os << "Current path is " << std::filesystem::current_path() << "\n";
 	}
 }
 //------------------------------------------------------------------------------
@@ -28,6 +45,7 @@ void move_track(iplayer::Player& player, std::ostream& os, std::istream& is)
 	std::size_t from;
 	std::size_t to;
 	if (is >> from >> to) {
+		os << "Moving track from " << from << " to " << to << "\n";
 		player.move(from, to);
 	} else {
 		os << "Invalid argument\n";
@@ -38,6 +56,7 @@ void remove_track(iplayer::Player& player, std::ostream& os, std::istream& is)
 {
 	std::size_t pos;
 	if (is >> pos) {
+		os << "Removing track " << pos << "\n";
 		player.remove(pos);
 	} else {
 		os << "Invalid argument\n";
@@ -59,35 +78,39 @@ void info_tracks(iplayer::Player& player, std::ostream&os, std::istream&)
 	player.info_tracks(os);
 }
 
-
 //------------------------------------------------------------------------------
 void remove_duplicate(iplayer::Player& player, std::ostream&, std::istream&)
 {
 	player.removeDuplicate();
 }
 //------------------------------------------------------------------------------
-void play(iplayer::Player& player, std::ostream&, std::istream&)
+void play(iplayer::Player& player, std::ostream& os, std::istream&)
 {
+	os << "Play\n";
 	player.play();
 }
 //------------------------------------------------------------------------------
-void pause(iplayer::Player& player, std::ostream&, std::istream&)
+void pause(iplayer::Player& player, std::ostream& os, std::istream&)
 {
+	os << "Pause\n";
 	player.pause();
 }
 //------------------------------------------------------------------------------
-void stop(iplayer::Player& player, std::ostream&, std::istream&)
+void stop(iplayer::Player& player, std::ostream& os, std::istream&)
 {
-	player.pause();
+	os << "Stop\n";
+	player.stop();
 }
 //------------------------------------------------------------------------------
-void next(iplayer::Player& player, std::ostream&, std::istream&)
+void next(iplayer::Player& player, std::ostream&os, std::istream&)
 {
+	os << "Next\n";
 	player.next();
 }
 //------------------------------------------------------------------------------
-void previous(iplayer::Player& player, std::ostream&, std::istream&)
+void previous(iplayer::Player& player, std::ostream&os, std::istream&)
 {
+	os << "Previous\n";
 	player.previous();
 }
 //------------------------------------------------------------------------------
@@ -95,6 +118,7 @@ void select(iplayer::Player& player, std::ostream& os, std::istream& is)
 {
 	std::size_t pos;
 	if (is >> pos) {
+		os << "Select " << pos << "\n";
 		player.select(pos);
 	} else {
 		os << "Invalid argument\n";
@@ -105,6 +129,7 @@ void set_repeat(iplayer::Player& player, std::ostream& os, std::istream& is)
 {
 	bool b;
 	if (is >> b) {
+		os << "Set repeat mode " << b << "\n";
 		player.setRepeatMode(b);
 	} else {
 		os << "Invalid argument\n";
@@ -115,6 +140,7 @@ void set_random(iplayer::Player& player, std::ostream& os, std::istream& is)
 {
 	bool b;
 	if (is >> b) {
+		os << "Set random mode " << b << "\n";
 		player.setRandomMode(b);
 	} else {
 		os << "Invalid argument\n";
@@ -131,6 +157,7 @@ struct Command
 //------------------------------------------------------------------------------
 const static std::map<std::string, Command> commands{
 	{"help", {showHelp}},
+	{"cd", {cd, " $directory"}},
 	{"add_track", {add_track, " $file ($pos)"}},
 	{"move_track", {move_track, " $from $to"}},
 	{"remove_track", {remove_track, " $pos"}},
@@ -142,9 +169,9 @@ const static std::map<std::string, Command> commands{
 	{"stop", {stop}},
 	{"next", {next}},
 	{"previous", {previous}},
-	{"select", {select, "$pos"}},
-	{"set_repeat", {set_repeat, "$bool"}},
-	{"set_random", {set_random, "$bool"}},
+	{"select", {select, " $pos"}},
+	{"set_repeat", {set_repeat, " $bool"}},
+	{"set_random", {set_random, " $bool"}},
 };
 
 //------------------------------------------------------------------------------
@@ -154,6 +181,7 @@ void showHelp(iplayer::Player&, std::ostream& os, std::istream&)
 	for (const auto& [key, value] : commands) {
 		os << "- " << key << value.extraParam << "\n";
 	}
+	os << "- exit\n";
 }
 
 } // namespace
@@ -165,8 +193,13 @@ namespace iplayer
 void Shell::run(std::istream& is, std::ostream& os)
 {
 	os << "Imaginary player\n";
+	{
+		std::stringstream ss(std::string("../../data"));
+		cd(*player, os, ss);
+	}
 	showHelp(*player, os, is);
 	while (true) {
+		os << "> ";
 		std::string line;
 		std::getline(is, line);
 		std::stringstream ss(line);
@@ -180,9 +213,9 @@ void Shell::run(std::istream& is, std::ostream& os)
 		}
 		auto it = commands.find(command);
 		if (it != commands.end()) {
-			it->second.func(*player, os, is);
+			it->second.func(*player, os, ss);
 		} else {
-			os << "invalid command";
+			os << "invalid command\n";
 		}
 	}
 }
