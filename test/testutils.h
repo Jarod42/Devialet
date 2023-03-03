1e8539b1cd016120767364653cfb0d660ccf6622
Jarod42 <Jarod42@users.noreply.github.com>
2023-03-03 15:52:34 +0100

    Add missing file.

---
+++ test/testutils.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "playlist.h"
+#include "trackheader.h"
+
+#include <vector>
+
+//------------------------------------------------------------------------------
+inline iplayer::TrackHeader makeTrack(std::size_t n)
+{
+	return {.filename = "file" + std::to_string(n),
+	        .title = "Title" + std::to_string(n),
+	        .duration{std::chrono::seconds(n)}};
+}
+
+//------------------------------------------------------------------------------
+inline iplayer::Playlist buildPlaylist(const std::vector<std::size_t>& v)
+{
+	iplayer::Playlist res;
+	for (std::size_t n : v) {
+		res.push_back(makeTrack(n));
+	}
+	return res;
+}
